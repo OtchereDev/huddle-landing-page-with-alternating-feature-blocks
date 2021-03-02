cfa9700bdb284fca8816a8ea89fac60250cbcf0c
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-02 09:45:18 +0000

    poj initialized

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        cPink: 'hsl(322, 100%, 66%)',
+        veryPaleCyan: 'hsl(193, 100%, 96%)',
+        veryDarkCyan: 'hsl(192, 100%, 9%)',
+        grayishBlue: 'hsl(208, 11%, 55%)'
+      },
+      fonts:{
+        body:'Open Sans',
+        heading:'Poppins'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
