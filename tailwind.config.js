aeaf740e6cdc8af72032bec66e6f37d25d34cea6
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-04 11:40:13 +0000

    final tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./public/index.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -9,9 +12,17 @@ module.exports = {
         veryDarkCyan: 'hsl(192, 100%, 9%)',
         grayishBlue: 'hsl(208, 11%, 55%)'
       },
-      fonts:{
+      fontFamily:{
         body:'Open Sans',
         heading:'Poppins'
+      },
+      backgroundColor: (theme) => theme('colors'),
+      backgroundImage: {
+        hero_d:"url('images/bg-hero-desktop.svg')",
+        hero_m:"url('images/bg-hero-mobile.svg')"
+      },
+      boxShadow: {
+        custom: '0px 0px 20px 1px rgb(235,251,255);'
       }
     },
   },
